8d7d018ef49904887174a220566b983449594329
AureaStar <aurea.starlight@gmail.com>
2025-08-06 19:25:05 -0300

    Adiciona DAO Generico

---
+++ src/main/java/com/faculdade/oo/dao/GenericDAO.java
@@ -0,0 +1,20 @@
+package com.faculdade.oo.dao;
+
+import java.util.List;
+
+public interface GenericDAO<T> {
+    
+    void salvar(T entidade) throws Exception;
+    
+    T buscarPorId(int id) throws Exception;
+    
+    List<T> listarTodos() throws Exception;
+    
+    void atualizar(T entidade) throws Exception;
+
+    void remover(int id) throws Exception;
+
+    boolean existe(int id) throws Exception;
+    
+    int obterProximoId() throws Exception;
+}
