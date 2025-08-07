8901629e8c10c41ecf8340abbabb4838452e99f6
giovana <giovana.maieli@codejr.com.br>
2025-08-06 22:07:56 -0300

    Adiciona modelo de forma de pagamento

---
+++ src/main/java/com/faculdade/oo/model/FormaPagamento.java
@@ -0,0 +1,24 @@
+package com.faculdade.oo.model;
+
+public enum FormaPagamento {
+    DINHEIRO("Dinheiro"),
+    CARTAO_CREDITO("Cartão de Crédito"),
+    CARTAO_DEBITO("Cartão de Débito"),
+    PIX("PIX"),
+    BOLETO("Boleto Bancário");
+    
+    private final String descricao;
+    
+    FormaPagamento(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
