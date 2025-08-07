1dee1bd5ec3fc8f0d7de19e6142786069de54836
AureaStar <aurea.starlight@gmail.com>
2025-08-07 00:18:57 -0300

    Correção de erro no comentário inicial

--- src/main/java/com/faculdade/oo/exceptions/UsuarioNaoAutenticadoException.java
+++ src/main/java/com/faculdade/oo/exceptions/UsuarioNaoAutenticadoException.java
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Giovana Maieli da Conceicao Livramento - 202365172A
  * Esthefany Emmanuele Silva Carvalho - 202365500B
  * Aurea Cunha Prado - 202365062AC
@@ -8,7 +8,7 @@ package com.faculdade.oo.exceptions;
 public class UsuarioNaoAutenticadoException extends AutenticacaoException {
     
     public UsuarioNaoAutenticadoException() {
-        super("USER_NOT_AUTHENTICATED", "Usuário não está autenticado");
+        super("USER_NOT_AUTHENTICATED", "UsuÃ¡rio nÃ£o estÃ¡ autenticado");
     }
     
     public UsuarioNaoAutenticadoException(String message) {
