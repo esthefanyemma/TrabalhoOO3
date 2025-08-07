f033cb1ae11c81c562a9be4bde08d05fcc238d34
Esthefany <esthefanyemmanuelesc@gmail.com>
2025-08-06 22:43:03 -0300

    Fazendo atualizações de metodos

--- src/main/java/com/faculdade/oo/Main.java
+++ src/main/java/com/faculdade/oo/Main.java
@@ -4,7 +4,5 @@ public class Main {
     
     public static void main(String[] args) {
         System.out.println("Bem-vindo ao Trabalho 3 de Orientação a Objetos!");
-        
-        // TODO: Implementar a lógica do projeto aqui
     }
 }
--- src/main/java/com/faculdade/oo/TrabalhoOficial.java
+++ src/main/java/com/faculdade/oo/TrabalhoOficial.java
@@ -1,5 +1,36 @@
 package com.faculdade.oo;
 
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+import com.faculdade.oo.util.InicializadorDados;
+import com.faculdade.oo.view.swing.LoginView;
+
 public class TrabalhoOficial {
-    
+
+    public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println("Erro ao configurar Look and Feel: " + e.getMessage());
+        }
+        
+        try {
+            System.out.println("=== SISTEMA DE GERENCIAMENTO DE FRANQUIAS ===");
+            System.out.println("Trabalho Prático de Orientação a Objetos - DCC025");
+            System.out.println("===============================================");
+            System.out.println("Iniciando interface gráfica...");
+            
+            InicializadorDados inicializador = new InicializadorDados();
+            inicializador.criarDadosExemplo();
+            
+            SwingUtilities.invokeLater(() -> {
+                new LoginView().setVisible(true);
+            });
+            
+        } catch (Exception e) {
+            System.err.println("Erro fatal no sistema: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
