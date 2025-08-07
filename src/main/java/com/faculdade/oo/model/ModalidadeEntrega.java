bf87abd15fc82b8ffdd523210c26a342dde4b4ad
giovana <giovana.maieli@codejr.com.br>
2025-08-06 22:08:41 -0300

    Adiciona modelo de modalidade de entrega

---
+++ src/main/java/com/faculdade/oo/model/ModalidadeEntrega.java
@@ -0,0 +1,22 @@
+package com.faculdade.oo.model;
+
+public enum ModalidadeEntrega {
+    RETIRADA_LOJA("Retirada na Loja"),
+    ENTREGA_DOMICILIO("Entrega a Domicílio"),
+    DRIVE_THRU("Drive Thru");
+    
+    private final String descricao;
+    
+    ModalidadeEntrega(String descricao) {
+        this.descricao = descricao;
+    }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+    
+    @Override
+    public String toString() {
+        return descricao;
+    }
+}
