9772c32d44e4152a2ee1f535009c66e9733b679e
giovana <giovana.maieli@codejr.com.br>
2025-08-06 22:10:14 -0300

    Adiciona modelo de usuario

---
+++ src/main/java/com/faculdade/oo/model/Usuario.java
@@ -0,0 +1,53 @@
+package com.faculdade.oo.model;
+
+public abstract class Usuario {
+    protected int id;
+    protected String nome;
+    protected String cpf;
+    protected String email;
+    protected String senha;
+    protected TipoUsuario tipo;
+    
+    public Usuario() {}
+    
+    public Usuario(int id, String nome, String cpf, String email, String senha, TipoUsuario tipo) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.email = email;
+        this.senha = senha;
+        this.tipo = tipo;
+    }
+    
+    // Getters e Setters
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
+    
+    public String getCpf() { return cpf; }
+    public void setCpf(String cpf) { this.cpf = cpf; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getSenha() { return senha; }
+    public void setSenha(String senha) { this.senha = senha; }
+    
+    public TipoUsuario getTipo() { return tipo; }
+    public void setTipo(TipoUsuario tipo) { this.tipo = tipo; }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Usuario usuario = (Usuario) obj;
+        return id == usuario.id;
+    }
+    
+    @Override
+    public int hashCode() {
+        return Integer.hashCode(id);
+    }
+}
